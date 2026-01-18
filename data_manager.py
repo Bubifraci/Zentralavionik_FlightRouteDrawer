32026b3a22458567394291b7213302cb44a27d4d
Daniel <daniel_2006@outlook.com>
2026-01-18 14:40:03 +0100

    First commit

---
+++ data_manager.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from pathlib import Path
+import gmplot
+
+zoom_level = 9
+
+BASE_DIR = Path(__file__).resolve().parent
+file_path = BASE_DIR / "flight_data.csv"
+
+def convertSI(df):
+    #ft/s to m/s
+    df['VELOCITY BODY X'] = df['VELOCITY BODY X'] * 0.3048
+    df['VELOCITY BODY Y'] = df['VELOCITY BODY Y'] * 0.3048
+    df['VELOCITY BODY Z'] = df['VELOCITY BODY Z'] * 0.3048
+
+    df['VELOCITY WORLD X'] = df['VELOCITY WORLD X'] * 0.3048
+    df['VELOCITY WORLD Y'] = df['VELOCITY WORLD Y'] * 0.3048
+    df['VELOCITY WORLD Z'] = df['VELOCITY WORLD Z'] * 0.3048
+
+    #ft/s/s to m/s/s
+    df['ACCELERATION BODY X'] = df['ACCELERATION BODY X'] * 0.3048
+    df['ACCELERATION BODY Y'] = df['ACCELERATION BODY Y'] * 0.3048
+    df['ACCELERATION BODY Z'] = df['ACCELERATION BODY Z'] * 0.3048
+
+    #ft to m
+    df['PLANE ALTITUDE'] = df['PLANE ALTITUDE'] * 0.3048
+
+    #kts to m/s
+    df['AIRSPEED TRUE'] = df['AIRSPEED TRUE'] * 0.514444
+    df['AIRSPEED INDICATED'] = df['AIRSPEED INDICATED'] * 0.514444
+
+    #ft/min to m/s
+    df['VERTICAL SPEED'] = df['VERTICAL SPEED'] * 0.3048 / 60
+
+    #Delete ground velocity -> not needed
+    df = df.drop('GROUND VELOCITY', axis=1)
+    return df
+
+df_raw = pd.read_csv(file_path, parse_dates=['timestamp'])
+df = convertSI(df_raw)
+
+latitude_list = list(df['PLANE LATITUDE'])
+longitude_list = list(df['PLANE LONGITUDE'])
+
+gmap = gmplot.GoogleMapPlotter(latitude_list[0], longitude_list[0], zoom_level)
+gmap.scatter(latitude_list, longitude_list, 'blue', size=50, marker=False)
+gmap.plot(latitude_list, longitude_list, 'blue', edge_width=2.5)
+gmap.draw("map.html")
